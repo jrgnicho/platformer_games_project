1400c60b1eea462fe6bc42754cb5b5eabe8a13ff
jrgnicho <jrgnicho@gmail.com>
2014-08-03 22:26:59 -0500

    removed import references for unused/moved modules

--- simple_platformer/src/simple_platformer/levels/__init__.py
+++ simple_platformer/src/simple_platformer/levels/__init__.py
@@ -1,4 +1,3 @@
 #renaming
 from platform import *
-from level import *
 from game_level import *
--- simple_platformer/src/simple_platformer/players/__init__.py
+++ simple_platformer/src/simple_platformer/players/__init__.py
@@ -1,4 +1,2 @@
 #renaming
-from player import *
-from animated_player import *
 from animatable_player import *
