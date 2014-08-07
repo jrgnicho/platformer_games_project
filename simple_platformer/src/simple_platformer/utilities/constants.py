8d186a2ba84912e91e584fb67e1c04d0f26ce1d5
ros-industrial <ros-industrial@domain.org>
2014-08-07 00:09:23 -0500

    added color codes for printing on stdout

--- simple_platformer/src/simple_platformer/utilities/constants.py
+++ simple_platformer/src/simple_platformer/utilities/constants.py
@@ -20,3 +20,12 @@ class GameProperties:
     
     FRAME_RATE = 60
     GRAVITY_ACCELERATION = 0.35 # pixels per second square
+    
+class TerminalColorCodes:
+    
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
