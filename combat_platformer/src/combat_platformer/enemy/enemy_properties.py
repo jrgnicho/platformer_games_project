240c0cbee04f0869dfc6be8ce1475f976f358a5d
ros-industrial <ros-industrial@domain.org>
2014-09-22 22:35:58 -0500

    added the 'EnemyProperties' class

---
+++ combat_platformer/src/combat_platformer/enemy/enemy_properties.py
@@ -0,0 +1,58 @@
+from pygame import Rect
+
+class EnemyProperties(object):
+    
+    # size
+    COLLISION_WIDTH = 40
+    COLLISION_HEIGH = 60
+    
+    # Movement Defaults
+    JUMP_SPEED = -10 # y axis points downwards
+    SUPER_JUMP_SPEED = -12
+    WALK_SPEED = 2
+    RUN_SPEED = 4
+    DASH_SPEED = 8    
+    MAX_X_POSITION_CHANGE = 8       
+    INERTIA_REDUCTION = 0.14    
+    MAX_MIDAIR_DASHES = 1
+    
+    # Environment thresholds
+    MAX_DISTANCE_FROM_EDGE = 0.80 # percentage of width
+    MIN_DISTANCE_FROM_EDGE = 0.10 # percentage of width
+    
+    HANG_RADIUS = 5 # radius of circle use to check if an edge is near for hanging during fall
+    HANG_DISTANCE_FROM_TOP = 12 # Used to set the distance from the top of the hanging platform to the top of the collision sprite
+    HANG_DISTANCE_FROM_SIDE = 0
+    
+    CLIMB_DISTANCE_FROM_TOP = -5
+    CLIMB_DISTANCE_FROM_SIDE = 0
+    
+    PATROL_AREA_RECTANGLE = pygame.Rect(0,0,500,100)
+    
+    def __init__(self):
+        
+        self.collision_width = EnemyProperties.COLLISION_WIDTH
+        self.collision_height = EnemyProperties.COLLISION_HEIGH
+        self.jump_speed = EnemyProperties.JUMP_SPEED
+        self.super_jump_speed = EnemyProperties.SUPER_JUMP_SPEED
+        self.run_speed = EnemyProperties.RUN_SPEED
+        self.walk_speed = EnemyProperties.WALK_SPEED
+        self.dash_speed = EnemyProperties.DASH_SPEED
+        self.max_x_position_change = EnemyProperties.MAX_X_POSITION_CHANGE
+        self.max_midair_dashes = EnemyProperties.MAX_MIDAIR_DASHES
+        self.inertial_reduction = EnemyProperties.INERTIA_REDUCTION
+        
+        self.max_distance_from_edge = EnemyProperties.MAX_DISTANCE_FROM_EDGE
+        self.min_distance_from_edge = EnemyProperties.MIN_DISTANCE_FROM_EDGE
+        
+        self.hang_radius = EnemyProperties.HANG_RADIUS
+        self.hang_distance_from_top = EnemyProperties.HANG_DISTANCE_FROM_TOP
+        self.hang_distance_from_side = EnemyProperties.HANG_DISTANCE_FROM_SIDE
+        
+        self.climb_distance_from_top = EnemyProperties.CLIMB_DISTANCE_FROM_TOP
+        self.climb_distance_from_side = EnemyProperties.CLIMB_DISTANCE_FROM_SIDE
+        
+        self.patrol_area_rect = EnemyProperties.PATROL_AREA_RECTANGLE
+        self.patrol_walk_time = 10
+        self.patrol_nap_time = 5
+        self.alert_time = 5
