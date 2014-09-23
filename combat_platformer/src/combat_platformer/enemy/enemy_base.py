1c38fe95e7a826ed9324eba82d869308d7aff9ac
ros-industrial <ros-industrial@domain.org>
2014-09-22 22:35:16 -0500

    modifies the 'properties' member to use an instance of the  'EnemyProperties' class

--- combat_platformer/src/combat_platformer/enemy/enemy_base.py
+++ combat_platformer/src/combat_platformer/enemy/enemy_base.py
@@ -1,7 +1,9 @@
 from combat_platformer.player import PlayerBase
+from combat_platformer.enemy import EnemyProperties
 
 class EnemyBase(PlayerBase):
     
     def __init__(self):
         PlayerBase.__init__(self)
+        self.properties = EnemyProperties()
         
