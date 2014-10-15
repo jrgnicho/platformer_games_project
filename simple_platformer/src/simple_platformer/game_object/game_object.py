85c132284fa5e4b8b67d491aa656f1af14eaa3c8
ros-industrial <ros-industrial@domain.org>
2014-10-14 21:13:34 -0500

    added properties for querying the game object's position in the screen

--- simple_platformer/src/simple_platformer/game_object/game_object.py
+++ simple_platformer/src/simple_platformer/game_object/game_object.py
@@ -2,46 +2,12 @@ import pygame
 from pygame.sprite import Sprite
 
 class GameObject(Sprite):
-    
-    class GameRectangle(pygame.Rect):
-        
-        """
-        This class is a variant of the pygame.Rect class which reimplements the properties
-        top and bottom so that it is in accordance with an y axis pointing up instead
-        of down as it is the case for the regular pygame.Rect class.  This will allow 
-        keeping the location of objects relative to a world (level) frame of reference
-         instead of screen coordinate system which is more difficult to maintain
-        """
-        
-        def __init__(self,top,left,width,height):
-            pygame.Rect.__init__(self,top,left,width,height)
-            
-#         @property
-#         def top(self):
-#             return self.centery + int(0.5*self.height)
-#             return self.y
-#         
-#         @top.setter
-#         def top(self,value):
-#             self.centery = value - int(0.5*self.height)
-#             self.y = value
-#             
-#         @property
-#         def bottom(self):
-#             return self.centery - int(0.5*self.height)
-#             return self.y - self.height
-#         
-#         @bottom.setter
-#         def bottom(self,value):
-#             self.centery = value + int(0.5*self.height)
-#             self.y = value + self.height            
-#             print "Bottom set to %i, top at %i"%(value,self.top)
-            
+                
     
     def __init__(self,centerx,centery,w,h,parent_object = None):
         Sprite.__init__(self)
         self.parent_object = parent_object
-        self.rect = GameObject.GameRectangle(0,0,w,h)
+        self.rect = pygame.Rect(0,0,w,h)
         self.rect.centerx = centerx
         self.rect.centery = centery
         
@@ -51,8 +17,7 @@ class GameObject(Sprite):
         if self.parent_object != None:
             sx = self.rect.x + self.parent_object.screen_x
         else:
-            sx = self.rect.x
-            
+            sx = self.rect.x            
         #endif
         
         return sx 
@@ -63,27 +28,35 @@ class GameObject(Sprite):
         if self.parent_object != None:
             sy = self.rect.y + self.parent_object.screen_y
         else:
-            sy = self.rect.y
-            
+            sy = self.rect.y            
         #endif
-        
+                
         return sy
     
     @property
     def screen_bottom(self):
-        return self.rect.height + self.screen_y
+        return self.rect.height + self.screen_y()
         
     @property
     def screen_top(self):
-        return -self.rect.height + self.screen_y
+        return -self.rect.height + self.screen_y()
     
     @property
     def screen_left(self):
-        return self.screen_x
+        return self.screen_x()
     
     @property
     def screen_right(self):
         return self.screen_x + self.rect.width
+    
+    @property
+    def screen_centerx(self):
+        return self.rect.centerx + self.screen_x()
+    
+    @property
+    def screen_centery(self):
+        return self.rect.centery + self.screen_y()
+            
     
     
         
