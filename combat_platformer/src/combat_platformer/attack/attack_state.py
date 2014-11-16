51874454eed9f73e9cd3fa858597c6fc27801149
jrgnicho <jrgnicho@gmail.com>
2014-11-16 11:29:30 -0700

    renamed a few methods, a BaseAttackState will be implemented with common functionality and Submachine actions will be done through pygame.events

--- combat_platformer/src/combat_platformer/attack/attack_state.py
+++ combat_platformer/src/combat_platformer/attack/attack_state.py
@@ -2,6 +2,7 @@ import pygame
 from simple_platformer.game_state_machine import State
 from simple_platformer.game_state_machine import SubStateMachine
 from simple_platformer.game_object import *
+from combat_platformer.player import PlayerActionKeys
 from combat_platformer.attack import *
 
 
@@ -10,16 +11,56 @@ class AttackState(SubStateMachine):
     
     class StateKeys(object):
         
-        ACTIVATE = 'ATTACK/ACTIVATE'
-        CONTINUE = 'ATTACK/CONTINUE'
+        ACTIVE = 'ATTACK_ACTIVE'
+        CONTINUE = 'ATTACK_CONTINUE'
     
-    class ActivateState(State):
+    class ActiveState(State):
         
         def __init__(self,game_object):            
-            State.__init__(self,AttackState.StateKeys.ACTIVATE)
+            State.__init__(self,AttackState.StateKeys.ACTIVE)
             self.__game_object__ = game_object
             self.__attacks__ = None
+            self.__attack_index__ = 0
+            self.__active_attack__ = None
             
+            # transition members
+            self.continue_next = False
+            self.finished = False
+            
+            self.add_action(AnimatableObject.ActionKeys.ANIMATION_FRAME_ENTERED,
+                            lambda : self.update_attack())
+            
+        def reset(self):
+            self.__attack_index__ = 0
+            self.__active_attack__ = None
+            
+        @attacks.setter
+        def attacks(self,attacks):
+            self.__attacks__ = attacks
+            
+        def enter(self):
+            if self.__attack_index__ < len(self.__attacks__):
+                self.__active_attack__ = self.__attacks__[self.__attack_index__]
+                self.__active_attack__.activate()
+        
+        def exit(self):
+            self.__attack_index__+=1
+            self.__active_attack__.deactivate()
+            
+            if self.__attack_index__ >= len(self.__attacks__):
+                self.__attack_index__ = 0
+            #endif
+            
+        def update_attack(self):
+            if self.__active_attack__:
+                self.__active__attack__.select_strike(self.__game_object__.animation_frame_index)
+            #endif
+            
+        def queue_attack(self):            
+            if self.__game_object__.animation_frame_index> 0.5*self.__active_attack__.get_strike_count():
+                self.continue_next = True
+            #endif       
+        
             
     class ContinueState(State):
         
@@ -27,6 +68,10 @@ class AttackState(SubStateMachine):
             State.__init__(self,AttackState.StateKeys.CONTINUE)
             self.__game_object__ = game_object
             self.__attacks__ = None
+            
+        @attacks.setter
+        def attacks(self,attacks):
+            self.__attacks__
             
     
     def __init__(self,key,game_object):
@@ -36,7 +81,7 @@ class AttackState(SubStateMachine):
         self.__attacks__ = None
         
         # creating states
-        self.__activate_state = AttackState.ActivateState(self.__game_object__)
+        self.__activate_state = AttackState.ActiveState(self.__game_object__)
         self.__continue_state = AttackState.ContinueState(self.__game_object__)
         
     
