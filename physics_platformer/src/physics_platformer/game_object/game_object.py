458c3aa7f9b276d87986493ca124aff8e9728e13
jrgnicho <jrgnichodevel@gmail.com>
2015-05-19 16:44:58 -0700

    added some members to game object

--- physics_platformer/src/physics_platformer/game_object/game_object.py
+++ physics_platformer/src/physics_platformer/game_object/game_object.py
@@ -1,3 +1,6 @@
+
+from direct.showbase import Loader
+
 from panda3d.core import LColor
 from panda3d.core import Vec3
 from panda3d.core import Vec4
@@ -18,22 +21,27 @@ Game Object class
 
 """
 class GameObject(NodePath):
+    DEFAULT_BOX_MODEL = Loader.loadModel('models/box.egg')
     
-    def __init__(self,size,mass, collision_shape = None ,visual = None):
-    
-        NodePath.__init__(self)
-        self.size_ = size
+    def __init__(self,name,size,mass, bt_collision_shape = None ,visual = None):    
+        
+        # instantiating to a bullet rigid body
+        NodePath.__init__(self,BulletRigidBodyNode(name + "-rigid-body"))
+        
+        # size
+        self.size_ = size        
         
         # set collision shape
-        self.collision_shape_ = collision_shape if collision_shape != None else self.createBoxShape(size)
+        collision_shape = collision_shape if collision_shape != None else self.createBoxShape(size)        
         
         # set visual
+        self.visual_nh_ = GameObject.DEFAULT_BOX_MODEL
         
         
     def createBoxShape(self,size):
         pass
     
-    def createBoxVisual(self,size):
-        pass
+    
+    
         
         
