4636325df3ea915569c497c50bfa579ecd9ccd78
jrgnicho <jrgnichodevel@gmail.com>
2015-10-08 13:00:38 -0500

    removed some unnecessary action entries

--- physics_platformer/src/physics_platformer/game_actions/character_actions.py
+++ physics_platformer/src/physics_platformer/game_actions/character_actions.py
@@ -5,11 +5,10 @@ class CharacterAction(Action):
   MOVE_RIGHT = CharacterAction('MOVE_RIGHT')
   MOVE_LEFT = CharacterAction('MOVE_LEFT')
   MOVE_UP = CharacterAction("MOVE_UP")
+  MOVE_DOWN = CharacterAction("MOVE_DOWN")
   JUMP = CharacterAction('JUMP')
-  FALL = CharacterAction('FALL')
   DASH = CharacterAction('DASH')
   HALT = CharacterAction('DASH_HALT')
-  LAND = CharacterAction('LAND')
   
   def __init__(self,key,animation_name = None):
     
