048e56bef2a363215a41f35659d8d15ab527fb7f
Jorge Nicho <jrgnichodevel@gmail.com>
2018-03-12 00:12:02 -0500

    minor simulation changes, reads width from cns file now
    added missing surfaces and edges to egg model, needs to set flag integers

--- physics_platformer/src/physics_platformer/game_level/level.py
+++ physics_platformer/src/physics_platformer/game_level/level.py
@@ -20,7 +20,7 @@ class Level(NodePath):
   __BOUND_THICKNESS_ = 10.0
   __BOUND_DEPTH_ = 1.0 # y direction
   __GRAVITY__ = Vec3(0,0,-14)
-  __PHYSICS_SIM_SUBSTEPS__ = 5
+  __PHYSICS_SIM_SUBSTEPS__ = 8
   __PHYSICS_SIM_STEPSIZE__ = 1.0/180.0
   
   def __init__(self,name,min_point, max_point):
